69c0c7014c7981e7094f2d9c7f6d4601f056af84
Richard Stibbard <richardstibbard@gmail.com>
2020-05-16 12:18:03 +0100

    Second commit

--- routes/web.php
+++ routes/web.php
@@ -13,32 +13,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('owners/create', "Owners@create");
+// Owners controller - View
+Route::get('/', "Owners@index");
+Route::get('owners/{owner}', "Owners@show");
 
+// Owners controller - Create
+Route::get('owners/create', "Owners@create");
 Route::post('owners/create', "Owners@createOwner");
 
-Route::get('animals/create', "Animals@create");
-
-Route::post('animals/create', "Animals@createAnimal");
-
-Route::get('/', "Owners@index");
-
+// Owners controller - Edit
 Route::get('owners/edit/{owner}', "Owners@edit");
-
 Route::post('owners/edit/{owner}', "Owners@editOwner");
 
-Route::get('owners/', "Owners@index");
+// Animals controller - View
+Route::get('animals/', "Animals@index");
+Route::get('animals/{animal}', "Animals@show");
 
-Route::get('owners/{owner}', "Owners@show");
+// Animals controller - Create
+Route::get('animals/create', "Animals@create");
+Route::post('animals/create', "Animals@createAnimal");
+
+// Animals controller - Edit
+Route::get('animals/edit/{animal}', "Animals@edit");
+Route::post('animals/edit/{animal}', "Animals@editAnimal");
+
+
+////// 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/about', function () {
     return view('about');
 });
-
-Route::get('animals/', "Animals@index");
-
-Route::get('animals/{animal}', "Animals@show");
-
-Route::get('animals/edit/{animal}', "Animals@edit");
-
-Route::post('animals/edit/{animal}', "Animals@editAnimal");
